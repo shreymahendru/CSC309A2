270dd16532830c4bb114625ec7219cefe10b0b95
System Administrator <root@inside-238-29.wireless.utoronto.ca>
2016-02-10 21:52:39 -0500

    added createBug function

--- A2/javascript.js
+++ A2/javascript.js
@@ -27,10 +27,84 @@ function reDraw()
             foods[i].drawFood(ctx);
     }
 }
-function creatBug()
-{
 
+
+function makeBug(x, y, color){
+    
+    color = color;
+    alpha = ".5";
+    
+    //http://www.w3schools.com/tags/canvas_globalalpha.asp
+    context.globalAlpha = alpha;
+    
+    /*-- Whiskers, legs and arms--*/
+    context.beginPath();
+    context.moveTo(x,y);
+    context.lineTo(x+5, y-15);
+    context.lineTo(x+10, y);
+    context.moveTo(x+5, y-20);
+    context.lineTo(x+4, y-22);
+    context.lineTo(x+6, y-22);
+    context.lineTo(x+5, y-20);
+    context.moveTo(x, y-20);
+    context.lineTo(x+10, y-40);
+    context.moveTo(x+10, y-20);
+    context.lineTo(x, y-40);
+    context.lineWidth = 2;
+    context.strokeStyle = color;
+    
+    /*-- Triangles on the tips --*/
+    context.moveTo(x,y);
+    context.lineTo(x, y-3);
+    context.lineTo(x+1.73, y-2.4);
+    context.lineTo(x, y);
+    context.moveTo(x+10, y);
+    context.lineTo(x+8.27, y-2.4);
+    context.lineTo(x+10, y-3);
+    context.lineTo(x+10, y);
+    context.moveTo(x, y-20);
+    context.lineTo(x, y-22);
+    context.lineTo(x+1.6, y-21.25);
+    context.lineTo(x, y-22);
+    context.moveTo(x+10, y-20);
+    context.lineTo(x+8.4, y-21.25);
+    context.lineTo(x+10, y-22);
+    context.lineTo(x+10, y-20);
+    context.moveTo(x, y-40);
+    context.lineTo(x, y-38);
+    context.lineTo(x+1.6, y-38.25);
+    context.lineTo(x, y-38);
+    context.moveTo(x+10, y-40);
+    context.lineTo(x+8.4, y-38.25);
+    context.lineTo(x+10, y-38);
+    context.lineTo(x+10, y-40);
+    context.stroke();
+    
+    /*-- Body parts --*/
+    context.beginPath();
+    context.arc(x+5, y-15, 5, 0, 2*Math.PI);
+    context.moveTo(x+5, y-21);
+    context.bezierCurveTo(x, y-20, x, y-30, x+5, y-38.75);
+    context.moveTo(x+5, y-21);
+    context.bezierCurveTo(x+10, y-20, x+10, y-30, x+5, y-38.75);
+    context.fillStyle = color;
+    context.lineWidth = 1;
+    context.strokeStyle = "#000000"
+    context.stroke();
+    context.fill();
+    
+    /*-- Eyes and Mouth --*/
+    context.beginPath();
+    context.arc(x+3.3, y-13.2, 1, 0, 2*Math.PI);
+    context.arc(x+6.75, y-13.2, 1, 0, 2*Math.PI);
+    context.fillStyle = "white";
+    context.fill();
+    context.beginPath();
+    context.arc(x+5, y-15, 2.5, 0, Math.PI, false);
+    context.stroke();
 }
+
+
 var Food  = function(x, y, eaten) {  //food object has x and y coordinates and if it is eaten or not
     this.x =x;
     this.y= y;
